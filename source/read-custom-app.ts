1cc72c39ae17b3951167877f6456f77297c7af2f
Henrik Hermansen <henrikhermansen@users.noreply.github.com>
2026-03-21 10:36:33 +0100

    Fix importing of custom apps on Windows (#88)

--- source/read-custom-app.ts
+++ source/read-custom-app.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import {pathToFileURL} from 'node:url';
 import {type ComponentType} from 'react';
 import {type AppProps} from './types.js';
 
@@ -19,7 +20,8 @@ export default async function readCustomApp(
 		}
 
 		const filePath = path.join(directory, file);
-		const m = (await import(filePath)) as AppExports;
+		const fileUrl = pathToFileURL(filePath);
+		const m = (await import(fileUrl.href)) as AppExports;
 
 		customApp = m.default;
 		break;
